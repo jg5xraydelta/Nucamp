a05323476fce4e89821e8abb5dfe5544ab0cfab9
Jared Gavin <jg5xraydelta@gmail.com>
2022-09-27 08:50:00 -0500

    put computer player into workflow

--- Skipbo-Nucamp-Project/game_display.py
+++ Skipbo-Nucamp-Project/game_display.py
@@ -1,15 +1,15 @@
-def display(dk, plyr1, plyr2):
+def display(dk, plyr0, plyr1):
+    # dk passes the deck attributes, plyr0 is the computer, plyr1 is the human player
     print("----------------------------------------------------")
     print("Draw pile:", dk.top_deck)
     print("Build piles:", dk.build)
     print("----------------------------------------------------")
-    print(plyr1.name, "Stock pile:", plyr1.stock[-1])
-    print(plyr1.name, "Hand:", plyr1.hand)
-    print(plyr1.name, "Discard piles:", plyr1.top_dcard)
+    print(plyr1.name, "Stock pile:", plyr0.stock[-1])
+    print(plyr1.name, "Discard piles:", plyr0.top_dcard)
     print("----------------------------------------------------")
-    print(plyr2.name, "Stock pile:", plyr2.stock[-1])
-    print(plyr2.name, "Hand:", plyr2.hand)
-    print(plyr2.name, "Discard piles:", plyr2.top_dcard)
+    print(plyr2.name, "Stock pile:", plyr1.stock[-1])
+    print(plyr2.name, "Hand:", plyr1.hand)
+    print(plyr2.name, "Discard piles:", plyr1.top_dcard)
     print("----------------------------------------------------/n")
 
 
--- Skipbo-Nucamp-Project/skipbo_workflow.py
+++ Skipbo-Nucamp-Project/skipbo_workflow.py
@@ -1,10 +1,16 @@
 import skipbo as skb
-
+import game_display as game
 
 while True:
     # Prompt player name and ask if they would like to play.
-    player_name = input("What is your name?")
-    print("Would you like to play a game", player_name + "?")
+    player1 = input("What is your name?")
+    player1 = skb.player()
+
+    # initiate computer player
+    player0 = "MrComputer"
+    player0 = skb.player()
+
+    print("Would you like to play a game", player1_name + "?")
     print("Don worry ", player_name, " the game is called skipbo!")
     response = input("Yes or No?")
 
@@ -17,10 +23,10 @@ while True:
     player_control = 0
 
     if num%2 == 0:
-        print(player_name, "goes first!")
+        print(player1, "goes first!")
         player_control = 1
     else:
-        print("Mr. Computer goes first... womp, womp, wooomp")
+        print(player0, "goes first... womp, womp, wooomp")
 
     # Generate the deck
     dek = skb.generate_deck_iter()
